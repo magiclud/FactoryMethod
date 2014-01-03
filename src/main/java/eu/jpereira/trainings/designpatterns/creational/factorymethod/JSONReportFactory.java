c663924dc63f30ff68a7a8ff797b60f9731e3324
magiclud <you@example.com>
2014-01-03 12:34:18 +0100

    bledy w klasie ReportGenerator

--- src/main/java/eu/jpereira/trainings/designpatterns/creational/factorymethod/JSONReportFactory.java
+++ src/main/java/eu/jpereira/trainings/designpatterns/creational/factorymethod/JSONReportFactory.java
@@ -1,22 +1,10 @@
 package eu.jpereira.trainings.designpatterns.creational.factorymethod;
 
-public class JSONReportFactory implements ReportFactory, Report {
+public class JSONReportFactory implements ReportFactory {
 
 	@Override
 	public Report createReport() {
 		return new JSONReport();
-	}
-
-	@Override
-	public void generateReport(ReportData data) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getReportContent() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
